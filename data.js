80221a8ab030b807075f90dd0ab5597c8e2c4ef3
becm23 <becm23series@gmail.com>
2025-05-11 16:28:55 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+  { name: "Shakib Al Hasan", role: "All-rounder" },
+  { name: "Tamim Iqbal", role: "Batsman" },
+  { name: "Mashrafe Mortaza", role: "Bowler" },
+  { name: "Mustafizur Rahman", role: "Bowler" },
+  { name: "Liton Das", role: "Wicket-keeper" }
+];
